fb5199c0b4f602dab806a03e61d40871203c44cf
TakuHazard <marwendot@gmail.com>
2020-05-12 22:24:37 -0700

    Course info. All parts completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = (props) =>{
+  console.log("Header props is " + props)
+  return(
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+const Part = (props) =>{
+  return(
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+const Content = (props) =>{
+  console.log(props)
+  console.log(props)
+  return(
+    <div>
+      <Part part = {props.parts[0].name} exercise = {props.parts[0].exercises} />
+      <Part part = {props.parts[1].name} exercise = {props.parts[1].exercises} />
+      <Part part = {props.parts[2].name} exercise = {props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  return(
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+
+
+}
+
+const square = p => p * p
+const t = [1,2,3]
+const tSquared = t.map(square)
+console.log(tSquared.toString())
+
+const App = () =>{ 
+  const course = {
+    name : 'Half Stack application development',
+   parts :[  
+    {
+      name : 'Fundementals of React',
+      exercises : 10
+    },
+    {
+      name : 'Using props to pass data',
+      exercises : 7
+    },
+    {
+      name: 'State of a component',
+      exercises : 14
+    }
+  ]
+  }
+console.log("Inside app with" + course)
+  return(
+    <div>
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
+    </div>
+  )
+}
+ReactDOM.render(<App />,document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
